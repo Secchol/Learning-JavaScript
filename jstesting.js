4d8e432ebc0f8630a8d43a5f71e42706b449731b
Sechkin <surenderordie@abv.bg>
2022-04-13 19:43:25 +0300

    Calculator

--- jstesting.js
+++ jstesting.js
@@ -2,21 +2,26 @@ let firstNum = 8
 let secondNum = 2
 document.getElementById("num1-el").textContent = firstNum
 document.getElementById("num2-el").textContent = secondNum
-let answerDoc = documnet.getElementById("num-el")
-let sum = 0
-function sum(){
-sum = firstNum+secondNum
+let resultDoc = document.getElementById("sum-el")
+
+
+function add(){
+let sum = firstNum+secondNum
+resultDoc.innerText = "Sum: "+ sum
 }
 function substract(){
-sum = firstNum-secondNum
+let sum = firstNum-secondNum
+resultDoc.innerText = "Sum: "+ sum
+
 }
  
 function multiply(){
-sum = firstNum*secondNum
+let sum = firstNum*secondNum
+resultDoc.innerText = "Sum: "+ sum
 }
 
 function divide(){
-sum = firstNum/secondNum
+let sum = firstNum/secondNum
+resultDoc.innerText = "Sum: "+ sum
 
 }
-answerDoc.textContent += sum
