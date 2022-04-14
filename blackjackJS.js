d0af93809cf09c9453022f515ed5acd52c9422d0
Sechkin <surenderordie@abv.bg>
2022-04-14 17:04:52 +0300

    Will repaire a bug later

--- blackjackJS.js
+++ blackjackJS.js
@@ -1,12 +1,15 @@
 let firstNum =8
 let secondNum = 10
 let sum = firstNum+secondNum
-if (sum<21){
-
-}
-else if (sum===21){
-
-}
-else{
-
+let messageDoc = document.GetItemById("message-el")
+function startGame(){
+    if (sum<21){
+    messageDoc.textContent = "Do you want me to pick a new card?"
+    }
+    else if (sum===21){
+    messageDoc.textContent = "YOU HAVE GOT BLACKJACK!"
+    }
+    else{
+    messageDoc.textContent = "You are out of the game!"
+    }
 }
